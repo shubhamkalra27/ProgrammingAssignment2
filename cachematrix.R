11aff65aa89492b0694a0389b18c458197a2c079
shubham <iam@shubhamkalra.com>
2015-12-23 16:17:23 -0500

    adding comments and tests

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,14 @@
 install.packages("MASS")
 library(MASS)
+## installing package MASS which includes the inverse function
 
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
-a <- rbind(c(1, -1/4), c(-1/4, 1))  
-a
 
-ainv <- solve(a)
 
-ainv %*% a
-
+## First function, makrCacheMatrix functions takes a matrix input, and returns a list with set, get and setInv and getInv functions
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -28,7 +25,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## second functions takes ainput the list outputted from previous function, checks for the datastored in the cache, and spits the inverse value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -43,14 +40,14 @@ cacheSolve <- function(x, ...) {
   m
 }
 
-a <- rbind(c(1, -1/4), c(-1/4, 1))
-a1 <- rbind(c(1, -1/4, 4.5,33,55), c(-1/4, 1,2,12,34))
-x <- makeCacheMatrix(a)
-x1 <- makeCacheMatrix(a1)
-p1 <- cacheSolve(x1)
-p <- cacheSolve(x)
 
-a1 %*% p1
+# tests
+#a <- rbind(c(1, -1/4), c(-1/4, 1))
+#a1 <- rbind(c(1, -1/4, 4.5,33,55), c(-1/4, 1,2,12,34))
 
-solve(a)
+#x <- makeCacheMatrix(a)
+#x1 <- makeCacheMatrix(a1)
+
+#p1 <- cacheSolve(x1)
+#p <- cacheSolve(x)
 
